f042b141268b1a9c02843458186d9bd88dca0aed
UkiOnna <sikirovtemirlan@mail.ru>
2019-09-02 16:31:23 +0600

    init

---
+++ TryMessanger/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TryMessanger.Models
+{
+    public class User
+    {
+        string Id { get; set; }
+        string Name { get; set; }
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
